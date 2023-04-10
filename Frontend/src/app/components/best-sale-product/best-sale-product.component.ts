5b44defba0e277e73864cb057b6d1177d6892a30
faithfulonoriobakpo <faithful.onoriobakpo@gmail.com>
2023-04-10 14:48:46 +0100

    implemented best sales products component

--- Frontend/src/app/components/best-sale-product/best-sale-product.component.ts
+++ Frontend/src/app/components/best-sale-product/best-sale-product.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/Product';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-best-sale-product',
   templateUrl: './best-sale-product.component.html',
   styleUrls: ['./best-sale-product.component.scss']
 })
-export class BestSaleProductComponent {
+export class BestSaleProductComponent implements OnInit {
+  name: string = "Best Sale Product"
+  bestSaleProducts: Product[] = [];
 
+  constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {
+    this.productService.getAllProducts().subscribe(products => this.bestSaleProducts = products);
+  }
 }
