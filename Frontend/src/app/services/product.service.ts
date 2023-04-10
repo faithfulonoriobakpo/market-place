d34745d0cb11c8f7273b9fe97e97b87174ed5c6a
faithfulonoriobakpo <faithful.onoriobakpo@gmail.com>
2023-04-10 12:54:51 +0100

    added getAllProducts and getProductById to product service

--- Frontend/src/app/services/product.service.ts
+++ Frontend/src/app/services/product.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { Product } from '../models/Product';
+import {HttpClient} from '@angular/common/http'
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
+  private productsUrl: string = '../../assets/products-data.json'
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAllProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.productsUrl);
+  }
+
+  getProductById(id: number): Observable<Product> {
+    return this.http.get<Product>(`${this.productsUrl}/${id}`);
+  }
 }
