baa984e84bd009a567e413ca385f97a95b7c14ff
faithfulonoriobakpo <faithful.onoriobakpo@gmail.com>
2023-04-11 06:56:59 +0100

    included route path and respective components to app routing module

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'products/:id', component:ProductDetailsComponent},
+  {path:'**', redirectTo:'/'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
