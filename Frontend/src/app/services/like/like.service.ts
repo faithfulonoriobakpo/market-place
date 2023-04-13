aa3c3106d5b94b5363d3c435687aa899932cf925
faithfulonoriobakpo <faithful.onoriobakpo@gmail.com>
2023-04-13 19:23:17 +0100

    added methods for like service

--- Frontend/src/app/services/like/like.service.ts
+++ Frontend/src/app/services/like/like.service.ts
@@ -4,6 +4,22 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class LikeService {
+  public likedProducts: number[] =[];
 
   constructor() { }
+
+  likeProduct(productId: number): void {
+    if(!this.likedProducts.includes(productId)) this.likedProducts.push(productId);
+  }
+
+  unlikeProduct(productId: number): void {
+    if(this.likedProducts.includes(productId)){
+      let index = this.likedProducts.indexOf(productId);
+      this.likedProducts.splice(index,1);
+    }
+  }
+
+  isLiked(productId: number): boolean {
+    return this.likedProducts.includes(productId);
+  }
 }
