a5116ab1bd4b40bb71050615356aab67f1ab02d6
faithfulonoriobakpo <faithful.onoriobakpo@gmail.com>
2023-04-13 07:51:27 +0100

    implemented products page

--- Frontend/src/app/components/product-details/product-details.component.ts
+++ Frontend/src/app/components/product-details/product-details.component.ts
@@ -3,17 +3,32 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { Subject, takeUntil } from 'rxjs';
 import { Product } from 'src/app/models/Product';
 import { ProductService } from 'src/app/services/product.service';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
-  styleUrls: ['./product-details.component.scss']
+  styleUrls: ['./product-details.component.scss'],
+  animations: [
+    trigger('fadeIn', [
+      state('void', style({ opacity: 0 })),
+      transition(':enter', [
+        animate('0.3s ease-in-out', style({ opacity: 1 }))
+      ])
+    ])
+  ]
 })
 export class ProductDetailsComponent implements OnInit {
   private ngUnsubscribe = new Subject<void>();
   product!: Product;
   products!: Product[];
   id!: number;
+  selectedProductImage!: string;
+  fadeInAnimation = 'void';
+  animationTriggered: boolean = false;
+  aboutActive: boolean = true;
+  productDescription!: string[];
+
 
   constructor(
     private route: ActivatedRoute,
@@ -30,9 +45,25 @@ export class ProductDetailsComponent implements OnInit {
       .subscribe({
         next: (res) => {
           this.products = res;
-          this.product = this.products.filter((item) => item.id === this.id)[0];
+          this.product = this.products?.filter((item) => item.id === this.id)[0];
+          this.selectedProductImage = this.product.images[0].main;
+          this.productDescription = this.product.description.split('.');
+          this.productDescription.pop();
         },
         error: (err) => console.log(err),
       });
+  }
+
+  selectImage(image: string){
+    this.animationTriggered = true;
+    this.fadeInAnimation = 'void';
+    setTimeout(() => {
+      this.selectedProductImage = image;
+      this.fadeInAnimation = 'fadeIn';
+    }, 250);
+  }
+
+  aboutOrReview():void{
+    this.aboutActive = this.aboutActive? false : true;
   }
 }
