8b3b78ccfe3d68df0ca92ab4f21e5b760bc0fee4
faithfulonoriobakpo <faithful.onoriobakpo@gmail.com>
2023-04-13 10:56:25 +0100

    created shopping basket component

---
+++ Frontend/src/app/components/shopping-basket/shopping-basket.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shopping-basket',
+  templateUrl: './shopping-basket.component.html',
+  styleUrls: ['./shopping-basket.component.scss']
+})
+export class ShoppingBasketComponent {
+
+}
