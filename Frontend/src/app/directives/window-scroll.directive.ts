e691552be4a5ab9e979228df6ae8b648cf33783c
faithfulonoriobakpo <faithful.onoriobakpo@gmail.com>
2023-04-13 09:48:25 +0100

    added window directive to manipulate navbar on window scroll

---
+++ Frontend/src/app/directives/window-scroll.directive.ts
@@ -0,0 +1,15 @@
+import { Directive, HostBinding, HostListener } from '@angular/core';
+
+@Directive({
+  selector: '[appWindowScroll]'
+})
+export class WindowScrollDirective {
+
+  constructor() { }
+
+  @HostBinding('class.navbar-scrolled') isScrolled : boolean = false;
+  @HostListener('window:scroll') onWindowScroll() {
+    this.isScrolled = window.scrollY >  0;
+  }
+
+}
