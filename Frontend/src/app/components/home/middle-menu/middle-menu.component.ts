947f8ad2b3e7dbe7b6f488e364c9e0a2b9f15118
faithfulonoriobakpo <faithful.onoriobakpo@gmail.com>
2023-04-14 14:59:47 +0100

    implemented carousel swipping with hammerjs

--- Frontend/src/app/components/home/middle-menu/middle-menu.component.ts
+++ Frontend/src/app/components/home/middle-menu/middle-menu.component.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import * as Hammer from 'hammerjs';
+import { NgbCarousel } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-middle-menu',
   templateUrl: './middle-menu.component.html',
   styleUrls: ['./middle-menu.component.scss']
 })
-export class MiddleMenuComponent {
+export class MiddleMenuComponent implements AfterViewInit {
+  @ViewChild('carousel', { static: false }) carousel!: NgbCarousel;
+  @ViewChild('carouselElement', { static: false }) carouselElement!: ElementRef;
+  hammer!: HammerManager;
+
+  ngAfterViewInit() {
+    this.hammer = new Hammer(this.carouselElement.nativeElement);
+    this.hammer.on('swipeleft', () => this.carousel.next());
+    this.hammer.on('swiperight', () => this.carousel.prev());
+  }
 }
