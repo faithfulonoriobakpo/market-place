5d2f2782145cc91c254295f2274e3d5fd2de0ae4
faithfulonoriobakpo <faithful.onoriobakpo@gmail.com>
2023-04-15 16:14:16 +0100

    removed unnecessary hostbinding decorator

--- Frontend/src/app/components/home/navbar/navbar.component.ts
+++ Frontend/src/app/components/home/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component,HostListener } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -6,19 +6,5 @@ import { Component,HostListener } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
-  navbarColor = 'transparent';
-  navbarPosition = 'fixed';
-
-  @HostListener('window:scroll', [])
-  onWindowScroll() {
-    const position = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
-    if (position >= 50) {
-      this.navbarColor = '#fff';
-      this.navbarPosition = 'fixed';
-    } else {
-      this.navbarColor = 'transparent';
-      this.navbarPosition = 'fixed';
-    }
-  }
 
 }
